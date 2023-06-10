14eefa0a2e4fe7c6ed142969dfe72c9e70e83053
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2023-06-10 16:33:50 +0200

    Add first platform generation

--- src/Camera.gd
+++ src/Camera.gd
@@ -2,26 +2,19 @@ extends Camera2D
 
 @export var player_path: NodePath
 var player
-var initial_player_position: Vector2
-
 var maxCameraY: float
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	make_current( )
 	player = get_node(player_path)
-	
-	# initial_player_position = player.global_position	
+
 	maxCameraY = global_position.y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var target_position = player.global_position
-	var camera_position = global_position
-
-	# if target_position.y < initial_player_position.y:
 	if target_position.y < maxCameraY:
 		maxCameraY = target_position.y
 		global_position.y = lerp(target_position.y, maxCameraY, 0.1) 
--- src/world.gd
+++ src/world.gd
@@ -5,11 +5,21 @@ var segments = [
 	# preload("res://segments/segmentB.tscn")
 ]
 
+var segment = preload("res://segments/segmentA.tscn")
+var width
 var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	width = get_viewport().get_visible_rect().size.x
 	randomize()
+	var y = 0
+	while y > -3000:
+		var new_platform = segment.instantiate()
+		new_platform.global_position = Vector2(randf_range(-width/2, width/2),y)
+		add_child(new_platform)
+		y-= randf_range(0, 210) # modify so x is between a fixed set 
+	# randomize()
 	# spawn_inst(0,0)
 	
 
