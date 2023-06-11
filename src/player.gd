a5febd668943a2f6ff4000f217b0116406399e51
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2023-06-11 13:46:33 +0200

    Add wrap-around for vertical jump and start end screen

--- src/player.gd
+++ src/player.gd
@@ -1,28 +1,61 @@
 extends CharacterBody2D
 
+@export var MOVE_SPEED = 200
+@export var JUMP_HEIGHT : float
+@export var JUMP_TIME_TO_PEEK : float
+@export var JUMP_TIME_TO_DESCENT : float
+@export var CAMERA_PATH : NodePath
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var JUMP_VELOCITY : float = ((2.0 * JUMP_HEIGHT) / JUMP_TIME_TO_PEEK) * -1.0
+@onready var JUMP_GRAVITY : float = ((-2.0 * JUMP_HEIGHT) / (JUMP_TIME_TO_PEEK * JUMP_TIME_TO_PEEK)) * -1.0
+@onready var FALL_GRAVITY : float = ((-2.0 * JUMP_HEIGHT) / (JUMP_TIME_TO_PEEK * JUMP_TIME_TO_DESCENT)) * -1.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var jump_speed = 2000
+var sprite
+var camera
+var width
 
+func _ready():
+	camera = get_node(CAMERA_PATH)
+	width = get_viewport_rect().size.x
+	sprite = get_node("Sprite2D")
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity.y += get_gravity() * delta
+	velocity.x = get_input_velocity() * MOVE_SPEED
+	
+	#if (Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("move_up")) and is_on_floor():
+	#	jump()
 	move_and_slide()
+
+func get_gravity() -> float:
+	return JUMP_GRAVITY if velocity.y < 0.0 else FALL_GRAVITY
+
+func jump():
+	velocity.y = JUMP_VELOCITY
+	
+func get_input_velocity() -> float:
+	var horizontal:= 0.0
+	
+	if Input.is_action_pressed("move_left"):
+		horizontal -= 1.0
+		sprite.set_flip_h(true)
+	if Input.is_action_pressed("move_right"):
+		horizontal += 1.0
+		sprite.set_flip_h(false)
+	return horizontal
+	
+func _on_collision(body):
+	if body.is_in_group('Paddles') and get_real_velocity().y > 0:
+		set_velocity(Vector2(0, -jump_speed))
+	pass
+
+
+func _on_exit_screen():
+	if global_transform.origin.x > camera.global_position.x and velocity.x > 0:
+		set_position(Vector2(-width/2, global_position.y))
+	if global_transform.origin.x < camera.global_position.x and velocity.x < 0:
+		set_position(Vector2(width/2, global_position.y))
+	if global_transform.origin.y < camera.global_position.y:
+		# trigger game over
+		pass
